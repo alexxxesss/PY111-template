b44773df037f792fc09c9086ee8552b28672b789
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2022-02-19 01:08:18 +0300

    b2_Taylor

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -1,6 +1,8 @@
 """
 Taylor series
 """
+import math
+import itertools
 from typing import Union
 
 
@@ -11,8 +13,14 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    exp = 1
+    delta = 0.0001
+    for n in itertools.count(1, 1):
+        exp_n = (x ** n)/math.factorial(n)
+        exp += exp_n
+        if delta > exp_n:
+            break
+    return exp
 
 
 def sinx(x: Union[int, float]) -> float:
