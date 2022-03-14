01fed1e3236aac2df39696c067672c690b31dd22
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2022-03-14 20:05:19 +0300

    g1_merge_sort

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import List
 
 
@@ -8,4 +9,37 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+
+    if len(container) > 1:
+        middle = len(container) // 2
+        left = container[:middle]
+        right = container[middle:]
+        sort(left)
+        sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                container[k] = left[i]
+                i += 1
+            else:
+                container[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            container[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            container[k] = right[j]
+            j += 1
+            k += 1
+
     return container
+
+
+arr = [randint(-100, 100) for _ in range(100)]
+sort(arr)
