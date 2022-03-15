1777932214c29ff7e0aa5d9dcd600efb825b08f1
alexxxesss <alexeenkoalexxx@yandex.ru>
2022-03-16 01:09:22 +0300

    e1_depth_first_search

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,5 +1,21 @@
 from typing import Hashable, List
+from collections import deque
 import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def draw_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
+
+    for edge in graph.edges:
+        nx.draw_networkx_edges(
+            graph, pos,
+            edgelist=[(edge[0], edge[1])], connectionstyle="arc3,rad=0.2"
+            )
+
+    plt.show()
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +26,24 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    draw_graph(g)
+
+    path_nodes = []  # полностью сгоревшие узлы
+    visit_nodes = {node: False for node in g.nodes}  # посещенные узлы
+
+    wait_nodes = []  # стек
+    wait_nodes.append(start_node)  # добавляем в стек / вершина справа
+    visit_nodes[start_node] = True  # при добавлении узла в очередь он считается посещенным
+
+    while wait_nodes:  # пока есть горящие узлы (очередь не пустая) будем зажигать :)
+        current_node = wait_nodes.pop()  # забираем горящий узел с конца очереди
+        neighbours = g[current_node]
+
+        for neighbour in neighbours:
+            if not visit_nodes[neighbour]:
+                wait_nodes.append(neighbour)
+                visit_nodes[neighbour] = True
+
+        path_nodes.append(current_node)  # полностью сожгли
+
+    return path_nodes
